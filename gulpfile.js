0689f2ce0b4b2feb5dd21cc9325c90df8e36f403
Shawn Coots <shawncoots@gmail.com>
2017-11-01 12:11:36 -0400

    Adding reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
-var gulp = require('gulp');
-var $    = require('gulp-load-plugins')();
+var gulp        = require('gulp');
+var $           = require('gulp-load-plugins')();
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var reload      = browserSync.reload;
 
 var sassPaths = [
   'bower_components/normalize.scss/sass',
@@ -10,16 +13,25 @@ var sassPaths = [
 gulp.task('sass', function() {
   return gulp.src('scss/app.scss')
     .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
+      includePaths: sassPaths
     })
       .on('error', $.sass.logError))
     .pipe($.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./scss/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+gulp.task('default', ['serve']);
